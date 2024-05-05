1afee4e0d063af820ab20dd1e6aff647bd4077c3
DracoInferno <gemknight1997@gmail.com>
2024-05-05 12:58:28 -0700

    update
    tacker in progress

--- website_tracker/metric_tracker.py
+++ website_tracker/metric_tracker.py
@@ -1,13 +1,13 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from pymongo.mongo_client import MongoClient
-from Users import user31 as user
+from pymongo import MongoClient
+from Users import user34 as user
 
 uri = "mongodb+srv://gemknight1997:InfernoFire1997@cluster0.u0s2mdw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = MongoClient(uri)
 db = client.your_database
+collection = db.metrics  # Change 'metrics' to your actual collection name
 
 def main():
     # Initialize browser
@@ -16,27 +16,26 @@ def main():
     # Navigate to your website 
     driver.get("http://localhost:3000/")
 
-    # metrics = []
     SAMPLE_SIZE = 10
     count = 0
+
+    # Initialize presence time
     start_time = time.time()
-    presence_time = start_time
-    user.userAction(driver)
-    
-    # Loop through your tests
+    presence_time = user.userAction(driver)
+
+    # Perform user action
+    # user.userAction(driver)
+
     while count < SAMPLE_SIZE:
-        metrics = []
-        
-        # Add iteration number, control or test group, user file name, and presence time to metrics
-        metrics.append({
+        metrics = {
             "Iteration Number": count + 1,
-            "Control or Test Group": "Control",  # You need to update this based on your experiment setup
+            "Control or Test Group": "Control",  # Update this based on your experiment setup
             "User File Name": "user16",  # Update this with the appropriate user file name
             "Presence Time (Seconds)": presence_time
-        })
+        }
 
         # Insert metrics into the database
-        db.collection.insert_many(metrics)
+        collection.insert_one(metrics)
 
         # Increment count and wait for some time
         count += 1
